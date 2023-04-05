da8c61632edafba8dc54aac155dd79ad3c3d5299
Adi Kurniawan <adiikurnia7921@gmail.com>
2023-04-05 23:51:50 +0700

    hampir beres

--- index.js
+++ index.js
@@ -16,8 +16,7 @@ app.use(express.json());
 
 // // ROUTE OTORISASI
 
-app.post("/api/booking", async (req, res) => {
-  console.log(req.body);
+app.post("/api/daftar", async (req, res) => {
   const results = await client.query(
     `SELECT * FROM akun WHERE username = '${req.body.user}'`
   );
@@ -27,7 +26,6 @@ app.post("/api/booking", async (req, res) => {
   } else {
     const salt = await bcrypt.genSalt();
     const hash = await bcrypt.hash(req.body.pass, salt);
-    console.log(hash);
     await client.query(
       `INSERT INTO akun VALUES ('${req.body.email}','${req.body.user}','${hash}','${req.body.telp}')`
     );
@@ -54,15 +52,18 @@ app.use((req, res, next) => {
       }
     }
     if (authorized) {
-      if (req.path.startsWith("/login") && req.me.username === "adi") {
+      if (
+        req.path.startsWith("/login") ||
+        (req.path.startsWith("/booking") && req.me.username === "adi")
+      ) {
         res.redirect("/admin");
-      } else if (req.path.startsWith("/login")) {
+      } else if (req.path.startsWith("/login") || req.path === "/") {
         res.redirect("/booking");
       } else {
         next();
       }
     } else {
-      if (req.path.startsWith("/login")) {
+      if (req.path.startsWith("/login") || req.path.startsWith("/")) {
         next();
       } else {
         if (req.path.startsWith("/api")) {
@@ -86,6 +87,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(express.json());
 // Untuk membaca body berformat JSON
 
 // Login (dapatkan token)
@@ -114,7 +116,32 @@ app.get("/api/me", (req, res) => {
   res.json(me);
 });
 
-// app.use(express.static("public"));
+app.post("/api/booking", async (req, res) => {
+  await client.query(
+    `INSERT INTO booking VALUES ('${req.body.name}','${req.body.email}','${req.body.instansi}','${req.body.alat_tes}','${req.body.jumlah}','${req.body.loan_date}','${req.body.payback_date}','${req.body.telp}')`
+  );
+  res.status(200);
+  res.send("Anda berhasil melakukan booking");
+});
+
+app.get("/api/booking", async (req, res) => {
+  const admin = await client.query(`SELECT * FROM booking`);
+  res.status(200);
+  res.json(admin.rows);
+});
+
+app.delete("/api/booking/:email", async (req, res) => {
+  await client.query(`DELETE FROM booking WHERE email='${req.params.email}'`);
+  res.status(200);
+  res.send("Booking telah dihapus");
+});
+
+app.get("/api/logout", (_req, res) => {
+  res.setHeader("Cache-Control", "no-store"); // khusus Vercel
+  res.clearCookie("token");
+  res.send("Logout berhasil.");
+});
+
 app.listen(3000, () => {
   console.log("Berhasil");
 });
